f7db2718911335e27070cacfd1f9430267e53abc
Lucas <lp172910@gmail.com>
2024-03-01 13:17:30 -0300

    Implement menu options for product detail

--- app/src/main/java/br/com/alura/orgs/ui/activity/ProductDetail.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductDetail.kt
@@ -3,6 +3,11 @@ package br.com.alura.orgs.ui.activity
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import br.com.alura.orgs.R
+import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.database.dao.ProductDAO
 import br.com.alura.orgs.databinding.ActivityProductDetailBinding
 import br.com.alura.orgs.ui.extensions.loadImage
 import br.com.alura.orgs.ui.model.Product
@@ -13,12 +18,17 @@ class ProductDetail : AppCompatActivity() {
     private val productDetailBinding by lazy {
         ActivityProductDetailBinding.inflate(layoutInflater)
     }
+
+    private lateinit var productDAO: ProductDAO
+    private lateinit var product: Product
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(productDetailBinding.root)
-        supportActionBar?.hide()
 
-        val product: Product? =
+        productDAO = AppDatabase.getDBInstance(this).productDAO()
+
+        val product =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getParcelableExtra("product", Product::class.java)
         } else {
@@ -26,11 +36,30 @@ class ProductDetail : AppCompatActivity() {
         }
 
         product?.let {
+            this.product = product
             productDetailBinding.activityProductDetailImage.loadImage(it.imageURL)
             productDetailBinding.activityProductDetailName.text = it.name
             productDetailBinding.activityProductDetailDescription.text = it.description
             productDetailBinding.activityProductDetailValue.text = NumberFormat.getCurrencyInstance(Locale("pt", "br")).format(it.value)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.product_detail_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.product_detail_menu_edit -> {
+
+            }
+            R.id.product_detail_menu_delete -> {
+                productDAO.removeProduct(product)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
