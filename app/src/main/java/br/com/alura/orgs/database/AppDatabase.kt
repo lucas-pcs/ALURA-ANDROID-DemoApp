16efdc9e3481182ba9bc6ebad0bb8eb32f1b481c
Lucas <lp172910@gmail.com>
2024-03-04 09:46:41 -0300

    Implement singleton pattern to db instance

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -1,6 +1,7 @@
 package br.com.alura.orgs.database
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -14,10 +15,16 @@ abstract class AppDatabase: RoomDatabase() {
     abstract fun productDAO(): ProductDAO
 
     companion object{
+        @Volatile
+        private lateinit var db: AppDatabase
         fun getDBInstance(context: Context): AppDatabase {
+            if(::db.isInitialized) return db
             return Room.databaseBuilder(context, AppDatabase::class.java, "orgs.db")
                 .allowMainThreadQueries()
-                .build()
+                .build().also {
+                    db = it
+                    Log.d("AppDatabase", "getDBInstance: db instance created")
+                }
         }
     }
 
